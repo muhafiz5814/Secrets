d920b1de6ce5a941f0985e427c74f492629a7651
Muhafiz Raza <muhafizraza1209@gmail.com>
2023-12-26 12:22:15 +0530

    Connect to mongoDB server and create userDB database and users collection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import bodyParser from "body-parser"
-
+import mongoose from "mongoose"
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,12 +10,38 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
 
+await mongoose.connect("mongodb+srv://muhafiz-admin:lPt9H23OyTJERuIz@secrets-cluster.l8cp30t.mongodb.net/userDB")
+
+const schema = mongoose.Schema
+
+const userSchema = new schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
 app.get("/", (req, res) => {
     res.render("home")
 })
 
 app.get("/register", (req, res) => {
     res.render("register")
+})
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    try {
+        newUser.save()
+        res.render("secrets")
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
 })
 
 app.get("/login", (req, res) => {
