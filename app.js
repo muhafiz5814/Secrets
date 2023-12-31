a3522a3240ea14520580b70567e878a7447e6ed6
Muhafiz Raza <muhafizraza1209@gmail.com>
2023-12-31 20:11:32 +0530

    Level 5 Authentication using Cookies and Sessions.

--- app.js
+++ app.js
@@ -2,28 +2,45 @@ import "dotenv/config"
 import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
-import bcrypt from "bcrypt"
+import passport from "passport"
+import session from "express-session"
+import passportLocalMongoose from "passport-local-mongoose"
 
 const app = express()
 const port = process.env.PORT || 3000
-const saltRounds = 10
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 await mongoose.connect(process.env.CONNECTING_STRING_USERDB)
 
 const schema = mongoose.Schema
 
 const userSchema = new schema({
-    email: String,
+    username: String,
     password: String
 })
 
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema)
+
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
 
 app.get("/", (req, res) => {
     res.render("home")
@@ -33,50 +50,67 @@ app.get("/register", (req, res) => {
     res.render("register")
 })
 
-app.post("/register", async (req, res) => {
-
-    const hash = await bcrypt.hash(req.body.password, saltRounds)
-
-    const newUser = new User({
-        email: req.body.username,
-        password: hash
-    })
-
-    try {
-        newUser.save()
-        res.render("secrets")
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(500)
-    }
-})
-
 app.get("/login", (req, res) => {
     res.render("login")
 })
 
-app.post("/login", async (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-
-    try {
-        const user = await User.findOne({email: username})
-        const matched = await bcrypt.compare(password, user.password)
-        
-        if (matched) {
-            res.render("secrets")
-        } else {
-            res.render("login")
-        }
-
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(404)
-    }
-})
 
 app.get("/submit", (req, res) => {
     res.render("submit")
+})
+
+app.get("/secrets", (req, res) => {
+    // Below isAuthenticated() method is from passport
+    if(req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.post("/register", (req, res) => {
+
+    // Below register() method is from passport-locao-mongoose module
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if(err) {
+            console.log(err)
+            res.redirect("/register")
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets")
+            })
+        }
+    })
+
+})
+
+app.post("/login", (req, res) => {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    // Below login() method is from passport
+    req.login(user, (err) => {
+        if(err) {
+            console.log(err)
+            res.redirect("/login")
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets")
+            })
+        }
+    })
+
+})
+
+app.get('/logout', (req, res, next) => {
+    // Below logout() method is from passport
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/')
+    })
 })
 
 app.listen(port, (req, res) => {
